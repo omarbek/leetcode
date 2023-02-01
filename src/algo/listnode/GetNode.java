c44ce417450f5bdb2dc1cff56682118bc396b795
omarbekdinassil <dinasil.omarbek@gmail.com>
2023-02-02 01:36:13 +0600

    listnode: 4. getNode (4*)
    * - in general

---
+++ src/algo/listnode/GetNode.java
@@ -0,0 +1,29 @@
+package algo.listnode;
+
+public class GetNode {
+
+    public static void main(String[] args) {
+        SinglyLinkedListNode llist = new SinglyLinkedListNode(1);
+        llist.next = new SinglyLinkedListNode(2);
+        llist.next.next = new SinglyLinkedListNode(3);
+        llist.next.next.next = new SinglyLinkedListNode(4);
+        llist.next.next.next.next = new SinglyLinkedListNode(5);
+
+        System.out.println(getNode(llist, 2));
+    }
+
+
+    public static int getNode(SinglyLinkedListNode llist, int positionFromTail) {
+        SinglyLinkedListNode runner = llist;
+        SinglyLinkedListNode curr = llist;
+
+        for (int i = 0; i < positionFromTail; i++) {
+            runner = runner.next;
+        }
+        while (runner.next != null) {
+            runner = runner.next;
+            curr = curr.next;
+        }
+        return curr.data;
+    }
+}
