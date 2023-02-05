180324b57e176ebf48fa735e0cb92c07af3ce9ac
omarbekdinassil <dinasil.omarbek@gmail.com>
2023-02-05 17:14:47 +0600

    listnode: 9. reverse (9*)
    * - in general

---
+++ src/algo/listnode/HasCycle.java
@@ -0,0 +1,28 @@
+package algo.listnode;
+
+public class HasCycle {
+    public static void main(String[] args) {
+        SinglyLinkedListNode llist = new SinglyLinkedListNode(1);
+        llist.next = new SinglyLinkedListNode(2);
+        llist.next.next = new SinglyLinkedListNode(3);
+        llist.next.next.next = new SinglyLinkedListNode(2);
+
+        System.out.println(hasCycle(llist));
+    }
+
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        if (head == null) {
+            return false;
+        }
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
