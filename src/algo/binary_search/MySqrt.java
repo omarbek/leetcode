3af9805abeb7a2eddac57e32e234395cc8093e80
omarbekdinassil <dinasil.omarbek@gmail.com>
2023-02-09 03:38:16 +0600

    binarysearch: 1. sqrt (16*)
    * - in general

--- src/algo/binary_search/MySqrt.java
+++ src/algo/binary_search/MySqrt.java
@@ -6,16 +6,18 @@ public class MySqrt {
     }
 
     public int mySqrt(int x) {
-        if (x == 0)
-            return 0;
-        int left = 1, right = x;
+        if (x == 0) {
+            return x;
+        }
+        int left = 1, right = Integer.MAX_VALUE;
         while (true) {
-            int mid = left + (right - left)/2;
-            if (mid > x/mid) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
                 right = mid - 1;
             } else {
-                if (mid + 1 > x/(mid + 1))
+                if ((mid + 1) > x / (mid + 1)) {
                     return mid;
+                }
                 left = mid + 1;
             }
         }
