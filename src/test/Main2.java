f4b11e43fc34506c383a6f3e8e067d1273cf34a7
omarbekdinassil <dinasil.omarbek@gmail.com>
2024-08-15 02:15:21 +0500

    update tasks

--- src/test/Main2.java
+++ src/test/Main2.java
@@ -1,36 +1,32 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Main2 {
+
+    //we have a HashMap<String, Integer> and need to write a method to remove entries with negative values
+
     public static void main(String[] args) {
-        //avg(2) -> 2
-        //avg(1) -> 1.5
-        //avg(6) -> 3
-        //avg(11) - > 6
-        Main2 main2 = new Main2();
-        System.out.println(main2.avg(2));
-        System.out.println(main2.avg(1));
-        System.out.println(main2.avg(6));
-        System.out.println(main2.avg(11));
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", -3);
+        removeNegativeValues(map);
+        System.out.println();
     }
 
-    List<Integer> list = new ArrayList<>(3);
-
-    private double avg(int n) {
-        list.add(n);
-        int count = list.size();
-        if(count>3){
-            count=3;
-        }
-        double sum = 0;
-        int c=0;
-        for (int i = list.size() - 1; i >= 0; i--,c++) {
-            if(c<3) {
-                sum += list.get(i);
+    private static void removeNegativeValues(Map<String, Integer> map) {
+        Set<String> set= new HashSet<>();
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            if(entry.getValue()<0){
+                set.add(entry.getKey());
             }
         }
-        return sum / count;
+        for(String s:set){
+            map.remove(s);
+        }
     }
 }
